16dd392d931cd473472214160706b04b23f37cc6
Andreas Nicolaou <Andreas@Andreass-iMac.fios-router.home>
2019-10-01 01:21:14 -0400

    Updated stylesheet. Need to work on survey page input capturing, validation, and modals.

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends= [
+    {
+        name: "Jack Napier",
+        profilePhoto: "https://i.imgflip.com/1ctchl.jpg",
+        scores: ["5", "1", "5", "1", "1", "5", "3", "4", "1", "3"]
+    }
+];
+
+module.exports = friends;
