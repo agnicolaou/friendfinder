035f6abd0ef9e76df38c91fd7421e9c497a23e99
Andreas Nicolaou <Andreas@Andreass-iMac.fios-router.home>
2019-10-02 22:55:01 -0400

    apiroutes.js API GET REQUEST Created

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,4 +2,12 @@
 
 var friendsArray = require('../data/friends.js');
 
+//Routing start
 
+module.exports = function (app) {
+    // API get request showing data posted as JSON form
+    app.get('/api/friends', function (req, res) {
+        res.json(friendsArray);
+    });
+
+   
